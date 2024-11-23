cff11a0bd590083c68226b4b8c004fcfb005c5ef
andersonfvinco <anderson.vinco@gmail.com>
2024-11-23 09:49:56 -0300

    ajuste do caminho do arquivo geo para funcionar no streamlit cloud

--- utils.py
+++ utils.py
@@ -13,9 +13,14 @@ AWS_REGION = "us-east-2"  # Ajuste conforme necessário
 
 def carrega_mapa():
     
-    uploaded_file = 'data/110m_cultural.zip'
-    gdf = gpd.read_file(f"zip://{uploaded_file}", layer="ne_110m_admin_0_countries") #ne_110m_populated_places, ne_110m_admin_0_countries
-    
+    #uploaded_file = 'data//110m_cultural.zip'
+    #gdf = gpd.read_file(f"zip://{uploaded_file}", layer="ne_110m_admin_0_countries") #ne_110m_populated_places, ne_110m_admin_0_countries
+
+    # Caminho absoluto baseado na localização do script
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_path, "data", "110m_cultural.zip")
+    gdf = gpd.read_file(f"zip://{file_path}", layer="ne_110m_admin_0_countries") #ne_110m_populated_places, ne_110m_admin_0_countries
+
     # Reprojetar para um CRS projetado (por exemplo, Mercator)
     gdf_projected = gdf.to_crs(epsg=3857)
     
